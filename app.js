f79c047b59d102079266587c436f6419125264f4
viewtifult <tashanduncan1994@gmail.com>
2022-06-10 10:40:54 +0100

    connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+
 const express = require('express')
 const app = express();
 const tasks = require('./routes/tasks')
-
+const connectDB = require('./db/connect')
+require('dotenv').config()
 // middleware
 
 app.use(express.json())
@@ -19,6 +21,15 @@ app.use('/api/v1/tasks', tasks)
 // app.patch('api/v1/tasks/:id') - update.task
 // app.delete('/api/v1/tasks/:id') -delete task
 
-
 const port = 3000
-app.listen(port, console.log(`server is listening on port ${port}...`))
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
