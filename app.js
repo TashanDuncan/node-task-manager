974be2f74e7a878ff5cc1e7a8dfd521ae93f47af
viewtifult <tashanduncan1994@gmail.com>
2022-06-15 15:14:22 +0100

    add custom 404

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 // middleware
 app.use(express.static('./public'))
 app.use(express.json())
@@ -12,13 +13,7 @@ app.use(express.json())
 
 
 app.use('/api/v1/tasks', tasks)
-
-// app.get('/api/v1/tasks')     -get all the tasks
-// app.post('api/v1/tasks')     -create a new task
-// app.get('/api/v1/tasks/:id)  -get a single task
-// app.patch('api/v1/tasks/:id') - update.task
-// app.delete('/api/v1/tasks/:id') -delete task
-
+app.use(notFound)
 const port = 3000
 
 const start = async () => {
